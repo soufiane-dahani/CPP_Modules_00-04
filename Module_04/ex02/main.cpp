81ee0bf441236756dce745466d7d009e4436b8aa
soufiane-dahani <dahanissoufiane@gmail.com>
2025-05-23 11:34:05 +0100

    ex02 is done .

--- Module_04/ex02/main.cpp
+++ Module_04/ex02/main.cpp
@@ -1,6 +1,20 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
 #include "Cat.hpp"
-int main()
-{
+#include <iostream>
+
+int main() {
+    std::cout << "Creating a Dog and Cat..." << std::endl;
+
+    Dog dog;
     Cat cat;
+
+    dog.makeSound();
     cat.makeSound();
+
+    std::cout << "\nTrying to instantiate Animal directly..." << std::endl;
+
+    //Animal animal;
+
+    return 0;
 }
