68bae068675196def3a0ccfce38ea5691369bead
soufiane-dahani <dahanissoufiane@gmail.com>
2025-05-23 12:43:28 +0100

    fix some error in last exe.. and start ex03

---
+++ Module_04/ex03/AMateria.cpp
@@ -0,0 +1,43 @@
+#include "AMateria.hpp"
+#include <iostream>
+
+AMateria::AMateria()
+{
+    _type = "Default";
+    std::cout << "Default constructor called for AMateria: " << _type << std::endl;
+}
+
+AMateria::~AMateria()
+{
+    std::cout << "Destructor called for AMateria: " << _type << std::endl;
+}
+
+AMateria::AMateria(std::string type)
+{
+    _type = type;
+    std::cout << "Parameterized constructor called for AMateria: " << type << std::endl;
+}
+
+AMateria::AMateria(const AMateria &other) 
+    : _type(other._type){
+        std::cout << "Copy constructor called for AMateria: " << _type << std::endl;
+    }
+AMateria& AMateria::operator=(const AMateria &other)
+{
+    if (this != &other)
+    {
+        std::cout << "Copy assignment operator called for AMateria" << std::endl;
+        this->_type = other._type;
+    }
+    return *this;
+}
+
+std::string const & AMateria::getType() const
+{
+    return _type;
+}
+
+void AMateria::use(ICharacter& target)
+{
+    (void)target;
+}
---
+++ Module_04/ex03/AMateria.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "ICharacter.hpp"
+class AMateria
+{
+    protected:
+        std::string _type;
+    public:
+        AMateria();
+        AMateria(std::string type);
+        AMateria(const AMateria &other);
+        AMateria& operator=(const AMateria &other);
+        virtual ~AMateria();
+        std::string const & getType() const;
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
